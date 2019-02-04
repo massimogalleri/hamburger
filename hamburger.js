3313dab2a0f11bf9983b4fecd7dc84ea38227fd3
Massimo Galleri <massimo@galleri.it>
2019-02-04 13:41:18 +0100

    Update hamburger.js

--- hamburger.js
+++ hamburger.js
@@ -1,43 +1,45 @@
-// vars
+{
 
-var _is_nav_open = false,
-    _body = document.querySelector('body'),
-    _window = document.documentElement,
-    _hero = document.getElementById('hero'),
-    _hamburger = document.getElementById('hamburger');
+    const _body = document.querySelector('body');
+    const _window = document.documentElement;
+    const _hero = document.getElementById('hero');
+    const _hamburger = document.getElementById('hamburger');
 
+    let _is_nav_open = false;
 
-// hamburger function
+    // hamburger function
 
-function _hamburger_click() {
+    function _hamburger_click() {
 
-    if (!_is_nav_open) {
-        _hamburger.classList.add('active');
-        _body.classList.add('no-scroll');
-        _is_nav_open = true;
-    } else {
-        _hamburger.classList.remove('active');
-        _body.classList.remove('no-scroll');
-        _is_nav_open = false;
+        if (!_is_nav_open) {
+            _hamburger.classList.add('active');
+            _body.classList.add('no-scroll');
+            _is_nav_open = true;
+        } else {
+            _hamburger.classList.remove('active');
+            _body.classList.remove('no-scroll');
+            _is_nav_open = false;
+        }
+
     }
 
-}
-
-_hamburger.addEventListener('click', function () {
-    _hamburger_click();
-});
+    _hamburger.addEventListener('click', function () {
+        _hamburger_click();
+    });
 
 
-// scroll function
+    // scroll function
 
-function _window_scroll() {
-    if (_window.scrollTop < _hero.clientHeight - _hamburger.clientHeight - _hamburger.offsetTop) {
-        _hamburger.classList.add('on-hero');
-    } else {
-        _hamburger.classList.remove('on-hero');
+    function _window_scroll() {
+        if (_window.scrollTop < _hero.clientHeight - _hamburger.clientHeight - _hamburger.offsetTop) {
+            _hamburger.classList.add('on-hero');
+        } else {
+            _hamburger.classList.remove('on-hero');
+        }
     }
+
+    _window_scroll();
+
+    window.addEventListener('scroll', _window_scroll);
+    
 }
-
-_window_scroll();
-
-window.addEventListener('scroll', _window_scroll);
